853088f9b3800b675d6684a3f9c9363ee71edb55
unknown <marcotelles123@gmail.com>
2020-05-07 21:04:44 -0300

    ajustado deleter

--- routes.js
+++ routes.js
@@ -8,5 +8,5 @@ routes.post('/rates', RateController.store);
 routes.delete('/rates/:id', RateController.delete);
 routes.get('/videos', VideosController.index);
 routes.post('/videos', VideosController.store);
-routes.delete('/videos/:id', RateController.delete);
+routes.delete('/videos/:id', VideosController.delete);
 module.exports = routes;
