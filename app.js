758e15107d39890906212d490761cf1d41782992
marcotelles123 <marcotelles123@gmail.com>
2021-08-24 22:17:55 -0300

    cors

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ try {
 } catch (e) {
     console.log('Error happend while connecting to the DB: ', e.message)
 }
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json());
 app.use(routes);
 
