27382042afdea173bcada477951d9c37132f4ab6
marcotelles123 <marcotelles123@gmail.com>
2021-09-14 23:12:10 -0300

    finalizado primeira parte da autenticação

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
@@ -7,22 +8,74 @@ const http = require('http');
 const app = express();
 const server = http.Server(app);
 
+
 try {
     mongoose.connect('mongodb+srv://sa:P@ssw0rd@cluster0-etowf.mongodb.net/test?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+    }).then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
     });
 } catch (e) {
     console.log('Error happend while connecting to the DB: ', e.message)
 }
-app.use(function(req, res, next) {
+
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 app.options('*', cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
 app.use(routes);
+
+
+const db = require("./models/index");
+const Role = db.role;
+
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'moderator' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
 
 server.listen(process.env.PORT || 3334);
--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const { Router } = require ('express');
+const { authJwt } = require("./middlewares");
 const RateController = require ('./controllers/RateController');
 const VideosController = require ('./controllers/VideosController');
 const AnnotationController = require ('./controllers/AnnotationController');
@@ -11,7 +12,7 @@ routes.delete('/rates/:id', RateController.delete);
 routes.get('/videos', VideosController.index);
 routes.post('/videos', VideosController.store);
 routes.delete('/videos/:id', VideosController.delete);
-routes.get('/annotation', AnnotationController.index);
+routes.get('/annotation', [authJwt.verifyToken], AnnotationController.index);
 routes.put('/annotation', AnnotationController.update);
 routes.post('/annotation', AnnotationController.create);
 routes.post('/lottery', LoteriasController.calculemyhits);
