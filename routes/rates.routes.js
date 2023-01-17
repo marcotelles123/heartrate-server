ece197bef853d9c800cc125439e549d3f7a5d62b
marcotelles123 <marcotelles123@gmail.com>
2023-01-16 22:00:15 -0300

    Bills branch (#31)
    * fixing date type and creating a date limit to delete a bill
    
    * added month in set as paid
    
    * added month in set as paid
    
    * fixing tests
    
    * refactoring vairable name
    
    * ajusting time
    
    * ajusting time
    
    * creating new endpoint for averages

--- routes/rates.routes.js
+++ routes/rates.routes.js
@@ -4,6 +4,7 @@ const RateController = require ('../controllers/rates.controller.js');
 const routes = Router();
 
 routes.get('/rates', RateController.index);
+routes.get('/averages', RateController.average);
 routes.post('/rates', RateController.store);
 routes.delete('/rates/:id', RateController.delete);
 
